a07efce6dab3b9673bbe12f71234bf05181d9fea
jaq-cat <jaq.cat@protonmail.ch>
2021-01-14 21:21:07 -0300

    get possible sexuality names lel

--- src/sexualities.rs
+++ src/sexualities.rs
@@ -10,6 +10,18 @@ enum Sexualities {
 
 impl fmt::Display for Sexualities {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self)
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Sexualities {
+    fn get_names(&self) -> Vec<String> {
+        match self {
+            Self::Gay => vec!["gay", "homosexual"],
+            Self::Lesbian => vec!["lesbian", "homosexual"],
+            Self::Bi => vec!["bi", "bisexual"],
+            Self::Pan => vec!["pan", "pansexual"],
+            Self::Ace => vec!["ace", "asexual"],
+        }
     }
 }
