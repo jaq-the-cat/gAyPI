7c1508a5e47d92f1366b311e6cc6270dfd06939c
jaq-cat <jaq.cat@protonmail.ch>
2021-01-14 21:13:56 -0300

    better http response

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 // Provides gay through an easy-to-use REST API!
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::*;
 use serde::Serialize;
 
 #[derive(Serialize)]
 struct Message(&'static str);
 
+#[get("/")]
 async fn hi(_: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().body(web::Json(Message("Hello")))
+    HttpResponse::Ok().json(Message("Hello"))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(hi)))
+    println!("Starting gAyPI server");
+    HttpServer::new(|| App::new().service(hi))
         .bind(("127.0.0.1", 8000))?
         .run()
         .await
