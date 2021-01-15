537cbf5d1462fff497bb7cb7c945db03c5206fbe
jaq-cat <jaq.cat@protonmail.ch>
2021-01-15 14:32:32 -0300

    list of genders/sexualities now :p

--- src/main.rs
+++ src/main.rs
@@ -22,23 +22,27 @@ async fn hi(_: HttpRequest) -> impl Responder {
 }
 
 #[get("/gender")]
-async fn gender(gender: web::Json<String>) -> impl Responder {
-    if !is_valid(&gender[..]) {
-        return HttpResponse::NotFound().json(map![
-            "error" => Errors::GenderNotFound
-        ]);
+async fn gender(genders: web::Json<Vec<String>>) -> impl Responder {
+    let mut response: Vec<Vec<&str>> = Vec::new();
+    for gender in genders.iter() {
+        if !is_valid(&gender[..]) {
+            return HttpResponse::NotFound().json(Errors::GenderNotFound);
+        }
+        response.push(flags!(&gender[..]));
     }
-    HttpResponse::Ok().json(flags!(&gender[..]))
+    HttpResponse::Ok().json(response)
 }
 
 #[get("/sexuality")]
-async fn sexuality(sexuality: web::Json<String>) -> impl Responder {
-    if !is_valid(&sexuality[..]) {
-        return HttpResponse::NotFound().json(map![
-            "error" => Errors::GenderNotFound
-        ]);
+async fn sexuality(sxs: web::Json<Vec<String>>) -> impl Responder {
+    let mut response: Vec<Vec<&str>> = Vec::new();
+    for sexuality in sxs.iter() {
+        if !is_valid(&sexuality[..]) {
+            return HttpResponse::NotFound().json(Errors::SexualityNotFound);
+        }
+        response.push(flags!(&sexuality[..]));
     }
-    HttpResponse::Ok().json(flags!(&sexuality[..]))
+    HttpResponse::Ok().json(response)
 }
 
 #[actix_web::main]
